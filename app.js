0f6f9cb67049571664c4d466adc44614d91c6b35
varsha <varshakumari370@gmail.com>
2023-07-06 15:24:49 +0530

    just commit

--- app.js
+++ app.js
@@ -1,25 +1,24 @@
-const express = require('express')
- const app= express()
- const port = process.env.PORT || 3000;
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
 
- app.get('/',(req, res) =>{
-    res.send(`<h1>This is my first code deployed on AWS.</h1>`);
- })
+app.get("/", (req, res) => {
+  res.send(`<h1>This is my first code deployed on AWS.</h1>`);
+});
 
- app.get('/product', (req, res) =>{
-    res.send([
-        {
-            productId:'1',
-            price:100
-        },
-        {
-            productId:'1',
-            price:100
-        }
+app.get("/product", (req, res) => {
+  res.send([
+    {
+      productId: "1",
+      price: 100,
+    },
+    {
+      productId: "2",
+      price: 100,
+    },
+  ]);
+});
 
-    ])
- })
-
- app.listen(port, ()=> {
-    console.log(`Demo app is running on port: ${port}`)
- })
+app.listen(port, () => {
+  console.log(`Demo app is running on port: ${port}`);
+});
